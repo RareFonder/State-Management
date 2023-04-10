2f18c46425c21b14b4d0cc5f20e4cbee3d036544
RareFonder <108814268+RareFonder@users.noreply.github.com>
2023-04-10 18:37:50 +0800

    1 file modified, 4 added

---
+++ script.js
@@ -0,0 +1,9 @@
+import Player from './player.js';
+
+window.onload = () => {
+  loading.style.display = 'none';
+  const canvas = document.getElementById('canvas1');
+  const ctx = canvas.getContext('2d');
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+}
