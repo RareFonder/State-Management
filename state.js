532c2c53045347fb149d9f45e8a50ea6484a581f
RareFonder <108814268+RareFonder@users.noreply.github.com>
2023-04-10 19:15:35 +0800

    2 files modified

---
+++ state.js
@@ -0,0 +1,35 @@
+export const states = {
+  STANDING_LEFT: 0,
+  STANDING_RIGHT: 1,
+};
+
+class State {
+  constructor(state) {
+    this.state = state;
+  }
+};
+class StandingLeft extends State {
+  constructor(player) {
+    super('STANDING LEFT');
+    this.player = player;
+  }
+  enter() {
+    this.player.frameY = 1;
+  }
+  handleInput(input) {
+    if (input === 'PRESS right') this.player.setState();
+  }
+};
+class StandingRight extends State {
+  constructor(player) {
+    super('STANDING RIGHT');
+    this.player = player;
+  }
+  enter() {
+    this.player.frameY = 0;
+  }
+  handleInput() {
+    if (input === 'PRESS left')
+  }
+};
+
